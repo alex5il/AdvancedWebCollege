b3a6ffe0e873a5d01dec7166f2207f85a3abc500
RoboticsAlmaliah2015 <robotics@spam4.me>
2016-01-15 18:54:38 +0200

    Stubs + links

---
+++ app/controllers/catalog/catalogDetailsController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+define(['app'], function (app) {
+
+    var catalogDetailsController = function ($scope) {
+
+    };
+
+    app.register.controller('CatalogDetailsController', ['$scope', catalogDetailsController]);
+
+});
---
+++ app/controllers/orders/orderCartController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+define(['app'], function (app) {
+
+    var orderCartController = function ($scope) {
+
+    };
+
+    app.register.controller('OrderCartController', ['$scope', orderCartController]);
+
+});
