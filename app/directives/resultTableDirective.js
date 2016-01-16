bda5404c62f753227a9cdbdc059fff52f104fd44
alex5il <alexkono91@gmail.com>
2016-01-16 12:26:40 +0200

    paging stuff

--- app/directives/resultTableDirective.js
+++ app/directives/resultTableDirective.js
@@ -12,10 +12,12 @@ define(['app', 'services/customersService', 'services/gamesService'], function (
             controller: function ($scope) {
                 console.log('result table loaded');
 
-                $scope.items = [{name:'alex', body:'bb'},{name:'alex', body:'bb'},{name:'alex', body:'bb'},
-                    {name:'alex', body:'bb'},{name:'alex', body:'bb'},{name:'alex', body:'bb'},
-                    {name:'alex', body:'bb'},{name:'alex', body:'bb'},{name:'alex', body:'bb'},
-                    {name:'alex', body:'bb'},{name:'alex', body:'bb'},{name:'alex', body:'bb'}];
+                $scope.items = [{name:'alex1', body:'bb'},{name:'alex2', body:'bb'},{name:'alex3', body:'bb'},
+                    {name:'alex4', body:'bb'},{name:'alex5', body:'bb'},{name:'alex6', body:'bb'},
+                    {name:'alex7', body:'bb'},{name:'alex8', body:'bb'},{name:'alex9', body:'bb'},
+                    {name:'alex10', body:'bb'},{name:'alex11', body:'bb'},{name:'alex12', body:'bb'},{name:'last', body:'bb'}];
+
+                $scope.itemsPerPage = 3;
 
                 $scope.totalItems = 64;
                 $scope.currentPage = 4;
