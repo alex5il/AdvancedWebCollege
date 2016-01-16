d0050fead811039a5ed06aa841f56f1739121c73
RoboticsAlmaliah2015 <robotics@spam4.me>
2016-01-16 19:17:05 +0200

    Css

--- app/services/homeService.js
+++ app/services/homeService.js
@@ -7,66 +7,38 @@ define(['app'], function (app) {
         var serviceBase = 'http://www.giantbomb.com/api/games/',
             homeFactory = {};
 
-        function display(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            $('#fountainG').toggle();
-            //    $.each(response.results, function (i, result) {
-            //        $('#games').append('<h2>Title</h2><h3>' + result.name + '</h3>' + result.description);
-            //});
-            console.log(response);
-            return response.results;
-
-        }
+        var result;
 
         homeFactory.getPage = function (pageIndex) {
             var url = "http://www.giantbomb.com/api/games/?api_key=e31a53182866e975585bd1891e5716df2864eebf&field_list=description,name&limit=1&sort=number_of_user_reviews:desc&platforms=94&filter=description:null&format=jsonp&offset=1&json_callback=JSON_CALLBACK";
-            //var url = 'https://angularjs.org/greet.php?callback=JSON_CALLBACK&name=Super%20Hero';
-            $http.jsonp(url).
-            success(function(data, status, headers, config) {
-                //what do I do here?
-                console.log(status);
-                console.log(headers);
-                console.log(config);
-                console.log(data);
-                data();
-                //console.log(data);
-            }).
-            error(function(data, status, headers, config) {
-                $scope.error = true;
+
+            $http({
+                method: 'JSONP',
+                url: serviceBase,
+                params: {
+                    api_key: 'e31a53182866e975585bd1891e5716df2864eebf',
+                    field_list: 'description,name',
+                    limit: '1',
+                    sort: 'number_of_user_reviews:desc',
+                    platforms: '94',
+                    filter: 'description:null',
+                    format: 'jsonp',
+                    json_callback: 'JSON_CALLBACK',
+                    offset: pageIndex - 1
+                },
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $('#fountainG').toggle();
+                $.each(response.data.results, function (i, result) {
+                    $('#games').append('<h2>Title</h2><h3>' + result.name + '</h3>' + result.description);
+                });
+
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log("Error in jsonp api call.");
             });
-
-
-
-
-            //return $http({
-            //    method: 'JSONP',
-            //    url: serviceBase,
-            //    params: {
-            //        api_key: 'e31a53182866e975585bd1891e5716df2864eebf',
-            //        field_list: 'description,name',
-            //        limit: '1',
-            //        sort: 'number_of_user_reviews:desc',
-            //        platforms: '94',
-            //        filter: 'description:null',
-            //        format: 'jsonp',
-            //        callback: 'display',
-            //        offset: pageIndex
-            //    },
-            //}).then(function successCallback(response) {
-            //    // this callback will be called asynchronously
-            //    // when the response is available
-            //    $('#fountainG').toggle();
-            //    //    $.each(response.results, function (i, result) {
-            //    //        $('#games').append('<h2>Title</h2><h3>' + result.name + '</h3>' + result.description);
-            //    //});
-            //    console.log(response);
-            //    return response.results;
-            //
-            //}, function errorCallback(response) {
-            //    // called asynchronously if an error occurs
-            //    // or server returns response with an error status.
-            //});
         };
 
         return homeFactory;
