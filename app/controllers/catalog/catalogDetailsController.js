387f2558a75ff2dcab0d35d0b271c00fec1c40ce
DanielFedo <thecrow34@gmail.com>
2016-01-18 08:29:49 +0200

    Refresh catalogDetails after edit!

--- app/controllers/catalog/catalogDetailsController.js
+++ app/controllers/catalog/catalogDetailsController.js
@@ -5,6 +5,7 @@ define(['app', 'services/gamesService'], function (app) {
     var catalogDetailsController = function ($scope, gamesService, $rootScope) {
         // Get game by id (which is name)
         $scope.game = $rootScope.params.game;
+        $scope.tempGame = angular.copy($scope.game);
     };
 
     app.register.controller('CatalogDetailsController', ['$scope', 'gamesService', '$rootScope', catalogDetailsController]);
